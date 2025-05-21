64504afa8671284986baa84ef3432f671be722ef
Matcha Doom <deansmatthewcharles@gmail.com>
2025-05-21 13:15:05 +0900

    Update script.js
    redo order of operations to maybe fix Chrome iOS flip not working

--- P986/script.js
+++ P986/script.js
@@ -10,16 +10,17 @@ function loadNextBatch() {
   for (let i = loadedCards; i < Math.min(loadedCards + batchSize, totalCards); i++) {
     const card = document.createElement('div');
     card.className = 'card';
-    card.innerHTML = `
-      <div class="card-inner">
-        <div class="card-back">
-          <img src="cards/fronts/${String(i).padStart(3, '0')}.webp" alt="Card ${i}">
-        </div>
-        <div class="card-front">
-          <img src="cards/backs/${String(i).padStart(3, '0')}_back.webp" alt="Card ${i} Back">
-        </div>
-      </div>
-    `;
+card.innerHTML = `
+  <div class="card-inner">
+    <div class="card-front">
+      <img src="cards/backs/${String(i).padStart(3, '0')}_back.webp" alt="Card ${i}">
+    </div>
+    <div class="card-back">
+      <img src="cards/fronts/${String(i).padStart(3, '0')}.webp" alt="Card ${i} Back">
+    </div>
+  </div>
+`;
+
 
     card.addEventListener('click', () => {
       card.classList.toggle('flipped');
